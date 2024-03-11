f28ddb75e5857e7558ea4fe445928f4c0bd3d3d5
sethlinares <sethlinares1@gmail.com>
2024-03-11 07:51:07 -0400

    added needed doc

--- 1_TORCH_TOKENIZER.py
+++ 1_TORCH_TOKENIZER.py
@@ -37,13 +37,22 @@ class ImageTokenizer(Dataset):
                                                 transforms.ToTensor(),
                                                 transforms.Normalize(mean, std)  # Use imagenet's mean and std by default for normalization
                                             ]))
+        
+
+        # Here are our learnable parameters. We wrap the tensors in nn.Parameter so that they are registered as parameters of the model and can be optimized through gradient descent
         self.projection_matrix = torch.nn.Parameter(torch.empty((patch_size**2) * 3, model_dimension))
         self.class_token_vector = torch.nn.Parameter(torch.empty(1, model_dimension))
+
+        # We also need to initialize the bias for the linear projection layer. We initialize it to a tensor of zeros because we want the model to learn the appropriate bias during training. Unless you have a good reason to do otherwise, you should always initialize the bias to zero so you don't introduce any unnecessary bias into the model.
         self.bias = torch.nn.Parameter(torch.zeros(model_dimension))
 
-        # Apply Xavier uniform initialization
+        # Apply Xavier uniform initialization which is a common initialization scheme for neural network weights. The main idea is to initialize the weights in such a way that the variance of the inputs is the same as the variance of the outputs. This helps to keep the gradients from exploding or vanishing.
+
+        # Its worth mentioning that depending on the model you use, you might want to use a different initialization scheme. For example, if you're using a ReLU activation function, you might want to use Kaiming initialization instead of Xavier, but for the purposes of this example, Xavier is fine.
+
         init.xavier_uniform_(self.projection_matrix)
         init.xavier_uniform_(self.class_token_vector)
+        
 
 
         
@@ -62,11 +71,32 @@ class ImageTokenizer(Dataset):
         
         patches = self.image_to_patches(image)
         projected_patches = self.project_patches(patches) 
+
+        # Add the class token to the projected patches
         patches_with_token = torch.cat([self.class_token_vector.unsqueeze(0), projected_patches], dim=0)
+
+        # Add the positional encodings to the patches which we need to do before we feed them into the transformer model
         patches_with_token += self.positional_encodings.unsqueeze(0) 
         return patches_with_token, label
         
     def generate_positional_encodings(self):
+
+        """
+        This method generates the positional encodings for the patches. The positional encodings are added to the projected patches before they are fed into the transformer model. As explained in the README, the positional encodings are a way to give the model information about the spatial relationships between the patches since the transformer model does not have any built-in notion of space or time. 
+
+        The formula for the positional encodings is:
+        PE(pos, 2i) = sin(pos / 10000^(2i / d_model))
+        PE(pos, 2i+1) = cos(pos / 10000^(2i / d_model))
+        where PE(pos, 2i) is the value of the positional encoding at position pos and dimension 2i, and d_model is the model's dimension.
+
+        With my implementation, I tried to keep the code as close to the original idea as possible. That being said, often you will see the div_term being calculated like so:
+        div_term = torch.exp(torch.arange(0, model_dimension, 2) * -(torch.log(torch.tensor(10000.0)) / model_dimension))
+
+        But personally, this is a bit hard to read and understand. So I decided to rewrite it in a way that is more intuitive to me as someone learning and the result is virtually the same. If you're worried, I've included a unit test in test.py to showcase that the two methods are equivalent.
+        
+        From there, we need to actually fill in the positional encodings. We do this by creating a tensor of zeros with the shape (1 + num_patches, model_dimension) at the top, and then filling in the tensor with the positional encodings by alternating between the sin and cos functions based on even and odd indices. We then return the tensor as the positional encodings.
+        """
+
         positional_encodings = torch.zeros((1 + self.num_patches, self.model_dimension))
         position = torch.arange(0, self.num_patches + 1, dtype=torch.float32).unsqueeze(1)
         div_term = 1 / (self.wave_length ** (torch.arange(0, self.model_dimension, 2, dtype=torch.float32) / self.model_dimension))
@@ -110,3 +140,4 @@ class ImageTokenizer(Dataset):
         where Y is the output, X is the input, W is the projection matrix and b is the bias.
         """
         return torch.matmul(patches, self.projection_matrix) + self.bias
+# %%
